9bee987c6b83ae23f6c38db0162434b796f0daa6
TonyHui <thlcly@163.com>
2016-04-17 14:08:03 +0800

    add profession usage of Gson

---
+++ GsonJsonParse/src/main/java/com/aaront/gson/adapter/UserTypeAdapter.java
@@ -0,0 +1,46 @@
+package com.aaront.gson.adapter;
+
+import com.aaront.gson.pojo.User;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+/**
+ * @author tonyhui
+ * @since 16/4/17
+ */
+public class UserTypeAdapter extends TypeAdapter<User> {
+    @Override
+    public void write(JsonWriter out, User value) throws IOException {
+        out.beginObject();
+        out.name("name").value(value.name);
+        out.name("age").value(value.age);
+        out.name("email").value(value.emailAddress);
+        out.endObject();
+    }
+
+    @Override
+    public User read(JsonReader in) throws IOException {
+        User user = new User();
+        in.beginObject();
+        while (in.hasNext()) {
+            switch (in.nextString()) {
+                case "name":
+                    user.name = in.nextString();
+                    break;
+                case "age":
+                    user.age = in.nextInt();
+                    break;
+                case "email":
+                case "email_address":
+                case "emailAddress":
+                    user.emailAddress = in.nextString();
+                    break;
+            }
+        }
+        in.endObject();
+        return user;
+    }
+}
---
+++ GsonJsonParse/src/main/java/com/aaront/gson/demo/ProfessionUsageManager.java
@@ -0,0 +1,13 @@
+package com.aaront.gson.demo;
+
+/**
+ * @author tonyhui
+ * @since 16/4/17
+ */
+public class ProfessionUsageManager {
+    public static void main(String[] args){
+
+    }
+
+
+}
